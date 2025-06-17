127cddc37c9bcd22842af7525e9193132e52cf1e
Golla-Rakesh21 <rakeshgolla21@gmail.com>
2025-06-17 10:17:06 +0530

    web dev practice 4 node packages

---
+++ day_4/index.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+// const fs=require("fs");
+// const path=require("path");
+// // console.log(__dirname);
+// // console.log(path.join(__dirname,"../index.js"));
+
+// const filepath=path.join(__dirname,"b.txt");
+// console.log(filepath);
+// fs.readFile(filepath,'utf-8',function(err,data){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(data);
+//     }
+// });
+
+
+console.log(chalk.blue("hello world"));
